87957349f3d9a6be35f4cc44b8b6edc2be3c63a4
Luther Tychonievich <lat7h@virginia.edu>
2021-12-27 11:31:42 -0500

    extract due dates from cal.yaml

--- cal2html.py
+++ cal2html.py
@@ -1,7 +1,7 @@
 import re, markdown, dateutil
 from datetime import timedelta, datetime
 from sys import stderr
-
+epoch = datetime(1970,1,1)
 
 def yamlfile(f):
     from yaml import load
@@ -318,69 +318,64 @@ END:VEVENT'''.format(sec=sec,
         ans.append('END:VCALENDAR')
         return '\n'.join(ans)
 
-# def slug2asgn(slug, group, raw):
-    # ans = {}
-    # ans.update(raw['assignments'].get('.groups').get(group,{}))
-    # ans.update(raw['assignments'][slug])
-    # return ans
+def slug2asgn(slug, group, raw):
+    ans = {}
+    ans.update(raw['assignments'].get('.groups').get(group,{}))
+    ans.update(raw['assignments'][slug])
+    return ans
     
 
-# def cal2assigments(cal,raw):
-    # ans = {}
-    # for week in cal:
-        # for day in week:
-            # if day is not None:
-                # for event in day['events']:
-                    # if event.get('kind') == 'assignment':
-                        # s = event['slug']
-                        # g = event['group']
-                        # dat = slug2asgn(s,g,raw)
-                        # dat['group'] = g
-                        # ans[s] = dict(**dat)
-    # return ans
+def cal2assigments(raw):
+    ans = {}
+    for k in sorted(raw['assignments'], key=lambda k:raw['assignments'][k].get('due',epoch)):
+        if k[0] == '.': continue
+        if 'group' not in raw['assignments'][k]:
+            raw['assignments'][k]['group'] = re.sub('[- 0-9]*$','',k)
+        ans[k] = slug2asgn(k, raw['assignments'][k]['group'], raw)
+    return ans
 
-# def coursegrade_json(data):
-    # groups = data['assignments'].get('.groups', {})
-    # weights, drops, inc, exc = {}, {}, {}, {}
-    # for k,v in groups.items():
-        # if 'portion' in v:
-            # weights[k] = v['portion']
-        # else:
-            # weights[k] = 0
-        # if type(weights[k]) is str:
-            # try:
-                # weights[k] = eval(weights[k].replace('%','/100'))
-            # except: pass
-        # if 'drop' in v:
-            # drops[k] = v['drop']
-        # if 'include' in v:
-            # inc[k] = v['include']
-        # if 'exclude' in v:
-            # exc[k] = v['exclude']
-    # for k,v in drops.items():
-        # if type(v) is str:
-            # v = eval(v.replace('%','/100'))
-        # if v < 1:
-            # cnt = 0
-            # for k,v in assignments_json(data).items():
-                # if v.get('group','') == k: cnt += 1
-            # v *= cnt
-        # drops[k] = int(round(v))
-    # return {'letters':[
-        # # {'A+':0.98},
-        # {'A' :0.93},
-        # {'A-':0.90},
-        # {'B+':0.86},
-        # {'B' :0.83},
-        # {'B-':0.80},
-        # {'C+':0.76},
-        # {'C' :0.73},
-        # {'C-':0.70},
-        # {'D+':0.66},
-        # {'D' :0.63},
-        # {'D-':0.60},
-        # {'F' :0.00},
-    # ],'weights':weights,'drops':drops,'includes':inc,'excludes':exc}
+def coursegrade_json(data):
+    groups = data['assignments'].get('.groups', {})
+    weights, drops, inc, exc = {}, {}, {}, {}
+    for k,v in groups.items():
+        if 'portion' in v:
+            weights[k] = v['portion']
+        else:
+            weights[k] = 0
+        if type(weights[k]) is str:
+            try:
+                weights[k] = eval(weights[k].replace('%','/100'))
+            except: pass
+        if 'drop' in v:
+            drops[k] = v['drop']
+        if 'include' in v:
+            inc[k] = v['include']
+        if 'exclude' in v:
+            exc[k] = v['exclude']
+    for k,v in drops.items():
+        if type(v) is str:
+            v = eval(v.replace('%','/100'))
+        if v < 1:
+            cnt = 0
+            for k,v in assignments_json(data).items():
+                if v.get('group','') == k: cnt += 1
+            v *= cnt
+        drops[k] = int(round(v))
+    return {'letters':[
+        # {'A+':0.98},
+        {'A' :0.93},
+        {'A-':0.90},
+        {'B+':0.86},
+        {'B' :0.83},
+        {'B-':0.80},
+        {'C+':0.76},
+        {'C' :0.73},
+        {'C-':0.70},
+        {'D+':0.66},
+        {'D' :0.63},
+        {'D-':0.60},
+        {'F' :0.00},
+    ],'weights':weights,'drops':drops,'includes':inc,'excludes':exc}
 
 if __name__ == '__main__':
     import os, os.path, glob
@@ -408,4 +403,9 @@ if __name__ == '__main__':
         with open('markdown/{}.ics'.format(sect), 'w') as fh:
             fh.write(cal.toICal(False,[sect]))
         
+    from pjson import prettyjson
+    with open('assignments.json','w') as fh:
+        fh.write(prettyjson(cal2assigments(raw)))
+    with open('coursegrade.json','w') as fh:
+        fh.write(prettyjson(coursegrade_json(raw)))
 
